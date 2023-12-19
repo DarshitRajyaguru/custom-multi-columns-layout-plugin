641aedf78def3dbcb6e75e2fa387fdf146bb8819
DarshitRajyaguru <darshitrajyaguru@gmail.com>
2023-12-19 10:47:42 +0530

    initial commit

---
+++ custom-multi-columns-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:         Custom Multi Columns Plugin
+ * Plugin URI:          https://example.com/
+ * Author:              Darshit Rajyaguru
+ * Author URI:          https://profiles.wordpress.org/darshitrajyaguru97/
+ * Description:         This plugin specially used for add a gutanberg block to your website.
+ * Version:             1.0.0
+ * Requires at least:   6.0
+ * Requires PHP:        7.4
+ * License:             GPL v2 or later
+ * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:         custom-multi-columns
+ * Langauges:           /languages
+ */
+
+ 
